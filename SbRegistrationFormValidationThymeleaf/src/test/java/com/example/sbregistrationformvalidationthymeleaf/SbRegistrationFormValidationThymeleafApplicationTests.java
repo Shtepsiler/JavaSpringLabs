755f7ed2a24f0ad06e422d86c6d86f6bd15b7075
Shtepsiler <100515332+Shtepsiler@users.noreply.github.com>
2023-12-03 13:15:52 +0200

    3

---
+++ SbRegistrationFormValidationThymeleaf/src/test/java/com/example/sbregistrationformvalidationthymeleaf/SbRegistrationFormValidationThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbregistrationformvalidationthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRegistrationFormValidationThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
